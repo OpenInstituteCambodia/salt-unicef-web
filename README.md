327dbc248f868b8695cdb088d74ed6d539e940af
phyrum <sphyrum@yahoo.com>
2018-03-20 15:29:49 +0700

    finish data synchronization API for mobile app

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 # API for checking User & Role
 # URL (local)
 localhost/api/user_role_app?email=ac@a.com&pwd=0123
+
+# API for Data synchronization
+localhost/api/sync_data_app
+# Sample of Post Json data to server for Synchronization
+[
+	{
+		"producers" : [
+					{"facility_id":"1","measurement_1":"20","measurement_2":"30"},
+					{"facility_id":"1","measurement_1":"10","measurement_2":"20"}
+			]
+	},
+	{
+		"monitors" : [
+					{"producer_id":"2","measurement":"20","warning" :"0"},
+					{"producer_id":"2","measurement":"10","warning" :"1"}
+			]
+	}
+]
--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\CustomHelper;
 use App\Http\Resources\UserRoleResource;
+use DB;
 
 class APIController extends Controller
 {
@@ -17,13 +18,69 @@ class APIController extends Controller
 
     public function SyncDataFromApp(Request $request)
     {
-        // Goal to insert data into DB
+        /**
+         * Example of json data
+            [
+                {
+                    "producers" : [
+                            {"measurement_1":"20","measurement_2":"30"},
+                            {"measurement_1":"10","measurement_2":"20"}
+                    ]
+                },
+                {
+                    "monitors" : [
+                            {"measurement":"20","warning" :"0"},
+                            {"measurement":"10","warning" :"1"}
+                    ]
+                }
+            ]
 
-        dd ($request);
+         * $request->toArray() // Converting JSON to Array
 
+         */
 
-
-        // API will inserted status back to mobile app in JSON format
-        //return new UserRoleResource($update_status);
+        DB::beginTransaction();
+        try{
+            foreach ($request->toArray() as $key1 => $val1)
+            {
+                // dd($key1) =0, dd($val1) = array of elements
+                foreach ($val1 as $key2 => $val2)
+                {
+                    // dd($key2)="producers" [table_name]; dd($val2) get array of table_inserted value
+                    foreach ($val2 as $data)
+                    {
+                            DB::table($key2)->insert($data);
+                    }
+                }
+            }
+            DB::commit();
+            return collect([
+                'code' => '200',
+                'message'=> "Ok"
+            ]);
+        }
+//        catch (ValidationException $e)
+//        {
+//            DB::rollBack();
+//            return collect([
+//                'message'=> $e->getMessage(),
+//            ]);
+//        }
+        catch (\Illuminate\Database\QueryException $e) {
+            // something went wrong with the transaction, rollback
+            DB::rollBack();
+            return collect([
+                'code' => '500',
+                'message'=> $e->getMessage(),
+            ]);
+        }
+        catch(\Exception $e)
+        {
+            DB::rollback();
+            return collect([
+                'code' => '500',
+                'message'=> $e->getMessage(),
+            ]);
+        }
     }
 }
